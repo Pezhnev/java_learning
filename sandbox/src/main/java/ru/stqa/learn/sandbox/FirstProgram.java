1811f9b05c0cffac9013a74653e95ffd1fcaa977
pezhnev <pezhnev@gmail.com>
2020-04-05 12:01:09 +0300

    Функция hello

--- sandbox/src/main/java/ru/stqa/learn/sandbox/FirstProgram.java
+++ sandbox/src/main/java/ru/stqa/learn/sandbox/FirstProgram.java
@@ -3,14 +3,25 @@ package ru.stqa.learn.sandbox;
 public class FirstProgram {
 	
 	public static void main(String[] args) {
-	System.out.println("hello, world!");
-		System.out.println(2+2);
-		System.out.println(2*2);
+	hello("world");
+	hello("Maxz");
 
-		System.out.println(1.0/2);
-		System.out.println(1/2.0);
-		System.out.println("2"+"5");
-		System.out.println(2+"5");
-		System.out.println("2+2=" + (2+3));
-}
+	double lon = 5;
+		System.out.println("ПЛощадь квадрата со стороной " +lon+"="+area(lon));
+
+		double a = 4;
+		double b =6;
+		System.out.println("Площадь промоугольника со сторонами " +a+ " и " +b+ "=" +area(a,b));
+	}
+	public static void  hello(String somebody) {
+		System.out.println("hello, "+somebody+  "!");
+	}
+
+	public  static  double area (double l) {
+		return l*l;
+	}
+
+	public  static  double area (double a, double b) {
+		return a*b;
+	}
 }
