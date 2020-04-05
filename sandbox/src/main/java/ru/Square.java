52b48fb09acdb7d9fcddac5abc05637dca23ea8f
pezhnev <pezhnev@gmail.com>
2020-04-05 13:38:24 +0300

    Создание классов

---
+++ sandbox/src/main/java/ru/Square.java
@@ -0,0 +1,9 @@
+package ru;
+
+public class Square {  //новый класс, структура объектов с информацией о квадрат
+  public double l; //атрибут
+
+  public Square(double l) { //псевдо функция-конструктор, без типа значения
+    this.l=l; // значения
+  }
+}
--- sandbox/src/main/java/ru/stqa/learn/sandbox/FirstProgram.java
+++ sandbox/src/main/java/ru/stqa/learn/sandbox/FirstProgram.java
@@ -1,27 +1,29 @@
 package ru.stqa.learn.sandbox;
 
+import ru.Rectangle;
+import ru.Square;
+
 public class FirstProgram {
 	
 	public static void main(String[] args) {
 	hello("world");
 	hello("Maxz");
 
-	double lon = 5;
-		System.out.println("ПЛощадь квадрата со стороной " +lon+"="+area(lon));
+Square s = new Square(5); // создаемновый объект типо квадрат с атрибутам
+		System.out.println("ПЛощадь квадрата со стороной " +s.l+"="+area(s));
 
-		double a = 4;
-		double b =6;
-		System.out.println("Площадь промоугольника со сторонами " +a+ " и " +b+ "=" +area(a,b));
+		Rectangle r = new Rectangle(4,6); //создаваемы объект типо прямоугольник с атрибутами
+		System.out.println("Площадь промоугольника со сторонами " +r.a+ " и " +r.b+ "=" +area(r));
 	}
 	public static void  hello(String somebody) {
 		System.out.println("hello, "+somebody+  "!");
 	}
 
-	public  static  double area (double l) {
-		return l*l;
+	public  static  double area (Square s) {  //функция area с параметром объекта типо квадрат
+		return s.l*s.l; //доступ к атрибуту
 	}
 
-	public  static  double area (double a, double b) {
-		return a*b;
+	public  static  double area (Rectangle r) {
+		return r.a*r.b;
 	}
 }
